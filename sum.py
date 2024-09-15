c73ebb785a90e65e2616fb92ae4836ef11368ddf
Ilay Guler <115847572+ilayG2004@users.noreply.github.com>
2024-09-15 11:21:47 -0400

    Update sum.py such that numbers are passed as arguments

--- sum.py
+++ sum.py
@@ -1,2 +1,7 @@
-sum = 2 + 2
-print(sum)
+def add(x,y):
+  return x+y
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+sum = add(num1,num2)
+print(num1, "+", num2, "=", sum)
